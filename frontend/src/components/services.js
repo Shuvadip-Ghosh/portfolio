c9b4b6237f682f59fab52f972159daf9c774feb7
Shuvadip Ghosh <shuvadipdeveloper@gmail.com>
2025-04-03 01:02:55 +0530

    solved the problem for manual scrolling in the services page.

--- frontend/src/components/services.js
+++ frontend/src/components/services.js
@@ -8,10 +8,7 @@ export default function Services() {
   React.useEffect(() => {
     el.current.scrollIntoView({ behavior: 'smooth' })
   });
-
-
   const [activeIndex, setActiveIndex] = useState(0);
-  const cardRefs = [useRef(null),useRef(null),useRef(null)]
   const services = [
     {
       title: 'Web Development',
@@ -47,13 +44,13 @@ export default function Services() {
                 <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0" />
               </svg>
             </div>
-            <p onClick={() => {setActiveIndex(index); cardRefs[index].current.scrollIntoView({ behavior: 'smooth',block: "center" })}}>Show more</p>
+            <p onClick={() => {setActiveIndex(index);}}>Show more</p>
           </div>
         ))}
         </div>
         <div className='servicedet'>
         {services.map((service, index) => (
-          <div id={`b-${index}`} ref={cardRefs[index]} className={`servicedetcard ${activeIndex === index ? 'servactdet' : ''}`}>
+          <div id={`d-${index}`}  className={`servicedetcard ${activeIndex === index ? 'servactdet' : ''}`}>
             <h1>{service.title}</h1>
             <h3>{service.description}</h3>
             <p>{service.details}</p>
